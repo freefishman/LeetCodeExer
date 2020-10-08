8d3ecca74ca54510c4efa97197bbe616218ed4d2
q2Lin <982703950@qq.com>
2020-10-08 20:50:28 +0800

    exercise

--- easy/66.加一.py
+++ easy/66.加一.py
@@ -1,15 +1,24 @@
 import math
 class Solution:
     def plusOne(digits):
-        num = ""
-        while digits[:]:
-            num += str(digits.pop(0))
-        num = int(num) + 1
-        num = str(num)
-        res = []
-        while num[:]:
-            res.append(int(num[:1]))
-            num = num[1:]
-        return res
+        # num = ""
+        # while digits[:]:
+        #     num += str(digits.pop(0))
+        # num = int(num) + 1
+        # num = str(num)
+        # res = []
+        # while num[:]:
+        #     res.append(int(num[:1]))
+        #     num = num[1:]
+        # return res
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] is not 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                if digits[0] is 0:
+                    digits.insert(0, 1)
+                    return digits
 
-print(Solution.plusOne([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,3]))
+print(Solution.plusOne([4,3,2,9]))
